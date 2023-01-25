add3b8bfc95393156ebf5102ecdd379e4a4770f1
Diego Sogari <diego.sogari@gmail.com>
2023-01-25 14:41:43 -0300

    [React] Use typescript template

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -3,18 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
-const client = new ApolloClient({
-  uri: 'http://localhost:8080/query',
-  cache: new InMemoryCache(),
-});
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
-  <ApolloProvider client={client}>
+  <React.StrictMode>
     <App />
-  </ApolloProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
