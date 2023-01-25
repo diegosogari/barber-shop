592ba2584a0d901a5746a8571d2bac133342193d
Diego Sogari <diego.sogari@gmail.com>
2023-01-25 12:16:42 -0300

    Add react app files

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,30 @@
 # syntax=docker/dockerfile:1
 
-# Build Stage
-FROM golang:1.19.5-alpine3.17 AS BuildStage
+# Node Build Stage
+FROM node:19-alpine3.16 AS NodeBuildStage
 WORKDIR /build
-COPY . .
+COPY ["react-app/package.json", "react-app/package-lock.json*", "./"]
+RUN npm install --production
+COPY react-app .
+RUN npm run build
+
+# Go Build Stage
+FROM golang:1.19.5-alpine3.17 AS GoBuildStage
+WORKDIR /build
 RUN apk add build-base
+COPY --from=NodeBuildStage /build/build react-app/build
+COPY ["go.mod", "go.sum*", "./"]
 RUN go mod download
+COPY . .
 RUN go build .
 
 # Deploy Stage
 FROM alpine:latest
 WORKDIR /app
-COPY --from=BuildStage /build/barber-shop .
-EXPOSE 8080
+COPY --from=GoBuildStage /build/barber-shop .
 RUN addgroup -S nonroot && adduser -S nonroot -G nonroot
-RUN mkdir /data
-RUN chown nonroot:nonroot /data
+RUN mkdir /data && chown nonroot:nonroot /data
 USER nonroot
+EXPOSE 8080
 ENTRYPOINT ["/app/barber-shop"]
+CMD [ "-db_filename=/data/barber.db" ]
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"embed"
 	"flag"
+	"io/fs"
 	"log"
 	"net/http"
 
@@ -13,16 +15,21 @@ import (
 	"gorm.io/gorm"
 )
 
+// Path to the database file
 var dbFilename string
+
+// Port to serve the GraphQL API
 var serverPort string
 
-func SetupFlags() {
+// Setup the command-line flags
+func setupFlags() {
 	flag.StringVar(&dbFilename, "db_filename", "data.db", "Path to the database file")
 	flag.StringVar(&serverPort, "server_port", "8080", "Port to serve the GraphQL API")
 	flag.Parse()
 }
 
-func SetupDatabase() {
+// Setup the database connection
+func setupDatabase() {
 	log.Printf("Opening database: %s", dbFilename)
 	dialector := sqlite.Open(dbFilename + "?_foreign_keys=on")
 	if db, err := gorm.Open(dialector, &gorm.Config{}); err != nil {
@@ -32,17 +39,26 @@ func SetupDatabase() {
 	}
 }
 
-func SetupServer() {
+//go:embed react-app/build
+var uiFS embed.FS
+
+// Setup the HTTP server
+func setupServer() {
 	config := generated.Config{Resolvers: &resolvers.Resolver{}}
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(config))
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/query", handler.NewDefaultServer(generated.NewExecutableSchema(config)))
+	http.Handle("/play", playground.Handler("GraphQL playground", "/query"))
+
+	if fs, err := fs.Sub(uiFS, "react-app/build"); err != nil {
+		log.Fatal("failed to get ui fs", err)
+	} else {
+		http.Handle("/", http.FileServer(http.FS(fs)))
+	}
 }
 
 func main() {
-	SetupFlags()
-	SetupDatabase()
-	SetupServer()
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", serverPort)
+	setupFlags()
+	setupDatabase()
+	setupServer()
+	log.Printf("Connect to http://localhost:%s", serverPort)
 	log.Fatal(http.ListenAndServe(":"+serverPort, nil))
 }
